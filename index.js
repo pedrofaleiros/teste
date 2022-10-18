6c28f3dae254ee307fd50de0a180e60b30d223c1
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2022-10-18 13:39:46 +0000

    grafo

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const Grafo = require('./grafo');
 const server = express();
+//yarn dev
 server.use(express.json()); // faz com que o express entenda JSON
+
 const users = [];
 
 server.get('/users', (req, res) => {
     return res.json(users);
 });
 
-server.post('/users', (req, res) => {
+server.post('/grafo', (req, res) => {
     const { name } = req.body;
 
     const num_v = req.body.vertices;
     const arestas = req.body.arestas;
-    console.log(arestas.length);
+
     var grafo = new Grafo(num_v);
 
     for(let i = 0; i < arestas.length; i++){
@@ -23,10 +25,7 @@ server.post('/users', (req, res) => {
         grafo.addAresta(y, x)
     }
 
-    grafo.mostraGrafo();
-
-
-    return res.json(users); // retorna a informação da variável users
+    return res.json(grafo); // retorna a informação da variável users
 });
 
 server.listen(3000);
